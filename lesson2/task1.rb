b60f62d3cb370373a43da2a35b658283c08ebd1e
Volodymyr Skichenko <vskichenko@gmail.com>
2016-05-18 00:28:46 +0300

    renamed variables

--- lesson2/task1.rb
+++ lesson2/task1.rb
@@ -2,16 +2,16 @@ class Task1
 	H = W = 4
 
 	def initialize
-  		@@arr = Array.new(H){Array.new(W){ rand(-10..10)}}
+  		@arr = Array.new(H){Array.new(W){ rand(-10..10)}}
 	end
 
 	def method1
-		puts "#{@@arr}"
-  		@@arr.flatten!.select!{|n| n < 0 }
-  		puts "#{@@arr}"
-  		puts @@arr.inject{|sum, n| sum + n}
+		puts "#{@arr}"
+  		@arr.flatten!.select!{|n| n < 0 }
+  		puts "#{@arr}"
+  		puts @arr.inject{|sum, n| sum + n}
 	end
 end
 
 obj = Task1.new
-puts obj.method1
+obj.method1

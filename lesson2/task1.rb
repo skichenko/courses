4c6e0548444932523c92c0a095eafdc95ee82dc3
Volodymyr Skichenko <vskichenko@gmail.com>
2016-05-18 00:04:45 +0300

    completed the task

---
+++ lesson2/task1.rb
@@ -0,0 +1,17 @@
+class Task1
+	H = W = 4
+
+	def initialize
+  		@@arr = Array.new(H){Array.new(W){ rand(-10..10)}}
+	end
+
+	def method1
+		puts "#{@@arr}"
+  		@@arr.flatten!.select!{|n| n < 0 }
+  		puts "#{@@arr}"
+  		puts @@arr.inject{|sum, n| sum + n}
+	end
+end
+
+obj = Task1.new
+puts obj.method1
---
+++ lesson2/task2.rb
@@ -0,0 +1,18 @@
+class Task2 
+	def initialize
+		@@arr = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
+	end
+	
+	def method1
+		@found_key = @@arr.keys.select{ |key| key =~ /^yes/}.size
+		puts @found_key		
+	end	
+
+	#def method2
+	#	puts @found_key ## puts @found_key.size
+	#end
+end	
+
+obj = Task2.new
+#puts obj.method2
+puts obj.method1
